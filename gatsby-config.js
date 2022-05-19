008b20adbcfb73e1ac7e9f19545a436f90c4991d
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-05-19 10:13:11 -0300

    Setting Google TagManager Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,34 @@
 module.exports = {
-  siteMetadata: {
-    title: `Topflex`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "CHANGE-IT-LATER"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Topflex`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }]
+    plugins: [
+        "gatsby-plugin-styled-components",
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: "CHANGE-IT-LATER",
+            },
+        },
+        {
+            resolve: "gatsby-plugin-google-tagmanager",
+            options: {
+                id: "GTM-TLB2GZZ",
+                includeInDevelopment: false,
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
