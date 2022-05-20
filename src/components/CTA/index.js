fa8de3562287808c83a4466a594d55ed31b5048d
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-05-19 23:24:57 -0300

    Adding whatsapp link

--- src/components/CTA/index.js
+++ src/components/CTA/index.js
@@ -4,7 +4,7 @@ import * as S from "./styles";
 
 const CTA = ({...props}) => {
     return (
-        <S.CTA href="#" marginTop={props.marginTop}> <span>Comprar Online</span>  <StaticImage layout="fixed" src="../../images/arrow_button.svg"  /></S.CTA>
+        <S.CTA href="https://api.whatsapp.com/message/MO3VD2KS2IZ3O1?autoload=1&app_absent=0" marginTop={props.marginTop}> <span>Comprar Online</span>  <StaticImage layout="fixed" src="../../images/arrow_button.svg"  /></S.CTA>
     )
 }
 
