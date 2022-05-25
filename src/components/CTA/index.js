5767f8eaf3babcab24ed7e632772b2f7352429f4
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-05-25 10:41:43 -0300

    Fixing Whatsapp link error

--- src/components/CTA/index.js
+++ src/components/CTA/index.js
@@ -4,7 +4,7 @@ import * as S from "./styles";
 
 const CTA = ({...props}) => {
     return (
-        <S.CTA href="https://api.whatsapp.com/message/MO3VD2KS2IZ3O1?autoload=1&app_absent=0" marginTop={props.marginTop}> <span>Comprar Online</span>  <StaticImage layout="fixed" src="../../images/arrow_button.svg"  /></S.CTA>
+        <S.CTA href="https://api.whatsapp.com/send?phone=5585989489182&text=Quero%20saber%20mais%20sobre%20os%20produtos%20Topflex" marginTop={props.marginTop}> <span>Comprar Online</span>  <StaticImage layout="fixed" src="../../images/arrow_button.svg"  /></S.CTA>
     )
 }
 
