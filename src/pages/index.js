758a035afedbfe70940f5e3841ae614a62d3713b
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-06-21 14:47:34 -0300

    Adding Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,21 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import "../styles/global.css"
+
+class Application extends React.Component {
+  render() {
+    return (
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Topflex</title>
+          <link rel="canonical" href="https://topflexoffice.com.br" />
+        </Helmet>
+      </div>
+    )
+  }
+}
 
 const IndexPage = () => {
   return (
